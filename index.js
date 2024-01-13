ded9856c206f48107d70400e22c9e1f105aba76f
MargueritaBouChahine <margg2004@hotmail.com>
2024-01-13 16:24:11 +0200

    added a file

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 300);
+}
 var circle = document.getElementById("circle");
 var uparrow = document.getElementById("uparrow");
 var downarrow = document.getElementById("downarrow");
