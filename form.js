9a415f9d07d4685c6cd9a8d6f5bae98cce7d52be
MargueritaBouChahine <margg2004@hotmail.com>
2024-01-15 22:18:30 +0200

    added a file

---
+++ form.js
@@ -0,0 +1,16 @@
+window.onload = () => {
+    const transition_el = document.querySelector('.transition');
+
+    setTimeout(() => {
+        transition_el.classList.remove('is-active');
+    }, 300);
+}
+
+function fillTheForm() {
+    window.location.href="form.html";
+    var pickedCar = document.getElementsByClassName("car1").value;
+    if (pickedCar === "ToyotaRAV-4"){
+        document.getElementById('carchoice').value = "Toyota Rav-4";
+    }
+
+}
